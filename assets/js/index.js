efce345c6646727463a81d1ef97bb2084ce84e35
codwerk <louis_widdy@live.fr>
2020-03-31 01:42:51 +0200

    end intégration responsive, burger menu

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+const selectElement = (s) => document.querySelector(s);
+/* const selectElement = function(s) {
+  document.querySelector(s);
+} */
+
+selectElement('.nav__burger').addEventListener('click', ()=> {
+  selectElement('.nav__menu').classList.toggle('is-active');
+})
+
+window.addEventListener('scroll', (e)=> {
+
+  const scrollValue = window.scrollY;
+
+  if(scrollValue > 0){
+    selectElement('.nav').classList.add('sticky');
+    selectElement('.nav__logo span').classList.add('fill');
+  }else {
+    selectElement('.nav').classList.remove('sticky');
+    selectElement('.nav__logo span').classList.remove('fill');
+  }
+
+})
